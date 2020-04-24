36e09f45b22a831acf3d425d41b0ca361ec60ef9
Atilla T. Reis <teixeira.atilla@gmail.com>
2020-04-24 14:37:18 -0300

    teste

--- mobile/services/api.js
+++ mobile/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api= axios.create({
-    baseURL:'http://192.168.2.6:3333'
+   // baseURL:'http://192.168.2.6:3333'
+   baseURL:'http://192.168.1.11:3333'
 });
 
 export default api;
